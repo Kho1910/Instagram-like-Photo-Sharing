dc40f3f54a8a5bee0d457c9ee56728324adcde27
Kho1910 <khoa48211@gmail.com>
2026-04-11 12:20:07 +0700

    bo sung them phan like,auth

--- backend/src/routes/interactionRoute.js
+++ backend/src/routes/interactionRoute.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
+const interactionController = require('../controllers/interactionController');
 
+router.post('/photos/:id/like, authMiddleware, interactionController.likePhoto');
+
+router.post('/photos/:id/unlike, authMiddleware, interactionController.unlikePhoto');
+
+module.exports = router;
