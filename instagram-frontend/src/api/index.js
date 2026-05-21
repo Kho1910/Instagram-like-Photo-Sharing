48c511792b29c679d87ed4a5f032d3e96f51bbaa
Kho <khoa48211@gmail.com>
2026-05-22 00:17:01 +0700

    fixing url1

--- instagram-frontend/src/api/index.js
+++ instagram-frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+  baseURL: 'https://project-zhqsf.vercel.app/api',
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' }
 })
